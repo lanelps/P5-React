d5700f91d7950d0a0f4e84f363dcf25cd8c8e151
Lane le Prevost-Smith <lanelps@gmail.com>
2019-03-23 16:16:02 +1300

    created a build package of the website for GHP

---
+++ build/precache-manifest.ac1ba91b483c43ab85cd674b07eb5b13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "687a784ff846cdbc4110",
+    "url": "/P5-React/static/js/runtime~main.5c5efbfa.js"
+  },
+  {
+    "revision": "f3785601b821260fdb2d",
+    "url": "/P5-React/static/js/main.3928572f.chunk.js"
+  },
+  {
+    "revision": "84d7c627e6aa06b4e47a",
+    "url": "/P5-React/static/js/2.ae3db934.chunk.js"
+  },
+  {
+    "revision": "f3785601b821260fdb2d",
+    "url": "/P5-React/static/css/main.ace2915f.chunk.css"
+  },
+  {
+    "revision": "80fd60f4f404696f873c9fdb2bf0af2c",
+    "url": "/P5-React/index.html"
+  }
+];
